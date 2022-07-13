426f2c2066158bf1ed14c67ce556d74c031b20a5
marsnebulasoup <marsnebulasoup@gmail.com>
2022-07-13 14:45:18 -0500

    Initial commit

---
+++ Bryniak.Ch10Lab.cpp
@@ -0,0 +1,13 @@
+/*
+Movie Statistics
+Author: Kevin Bryniak
+Status: Incomplete
+Completed: N/A
+This program prompts the user for the number of students surveyed and how many movies they watched,
+then computes the average number of movies watched per student.
+*/
+
+int main()
+{
+  return 0;
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# COSC-1337
+## Chapter 10 Lab Assignment
+
+### Movie Statistics
+Prompts the user for the number of students surveyed and how many movies they watched, then computes the average number of movies watched per student.

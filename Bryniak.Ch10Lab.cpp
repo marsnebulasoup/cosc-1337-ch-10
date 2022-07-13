32952e2b0b743588188b63838a6a26ed21822919
marsnebulasoup <marsnebulasoup@gmail.com>
2022-07-13 14:52:14 -0500

    Added program constants

--- Bryniak.Ch10Lab.cpp
+++ Bryniak.Ch10Lab.cpp
@@ -13,6 +13,10 @@ then computes the average number of movies watched per student.
 
 using namespace std;
 
+const string TITLE = "Movie Statistics Calculator";
+const string STUDENT_COUNT_PROMPT = "Enter the number of students surveyed: ";
+const string STUDENT_MOVIES_PROMPT = "Enter the number of movies each watched by each student.";
+ 
 struct Student {
   int moviesWatched;
 
